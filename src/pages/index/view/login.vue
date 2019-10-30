2bb2ee9c2de88815b5e1467bea4616fdc96f56ef
yangguowei <yangguowei@kuaizi.co>
2019-10-30 15:23:04 +0800

    fix

--- src/pages/index/view/login.vue
+++ src/pages/index/view/login.vue
@@ -4,12 +4,18 @@
 
 <script>
 import count from '../components/count/index.vue'
+import trim from 'lodash/trim'
 export default {
   components: {
     count
   },
   data () {
     return {
+    }
+  },
+  methods: {
+    test () {
+      console.log(trim('aaa '))
     }
   }
 }
