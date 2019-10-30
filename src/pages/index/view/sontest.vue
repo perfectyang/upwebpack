e2691182a46a932354f73a009953605e4710238c
yangguowei <yangguowei@kuaizi.co>
2019-10-30 15:27:55 +0800

    fix

---
+++ src/pages/index/view/sontest.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="">
+    <div class="" v-for="elem in elemArray" :key="elem.key">
+
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+      }
+    }
+  }
+</script>
+
+<style>
+</style>
