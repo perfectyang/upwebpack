b7fcc9d3cbad64bfa25d55c607f37fb34ff0e8fd
yangguowei <yangguowei@kuaizi.co>
2019-10-30 15:32:01 +0800

    a

---
+++ src/pages/index/view/test.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="">
+    bbbbcccaa
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      name: '大哥',
+      good: ''
+    }
+  },
+  created () {
+    this.gosay()
+  },
+  methods: {
+    async gosay () {
+      let res = await this.test()
+      this.good = res.name
+    },
+    test () {
+      console.log('sss')
+      return {
+        name: 'aa'
+      }
+    }
+  }
+}
+</script>
+
+<style>
+</style>
